6f735b5d5d65076ffa815e7b08bafc60a8e2a355
BuildTools <unconfigured@null.spigotmc.org>
2019-11-18 23:50:10 +0900

    added html and sass loader, changed webpack setting to extract css file, cache babel-loader to start fast, changed mode to production to minify javascript and css

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "index.css";
+import "index.scss";
 import Hello from "components/Hello";
 
 class App extends React.Component {
