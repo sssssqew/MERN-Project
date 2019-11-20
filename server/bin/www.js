f1ced7fb992398d1487dd778375a93a2ed754dca
BuildTools <unconfigured@null.spigotmc.org>
2019-11-20 16:58:08 +0900

    applied clustering for server

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,6 +1,22 @@
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+
 const app = require("../index");
 const PORT = 5000;
 
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
-});
+if (cluster.isMaster) {
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died.`);
+    cluster.fork();
+  });
+  cluster.on("listening", (worker, address) => {
+    console.log(`Worker started with PID ${worker.process.pid}`);
+  });
+} else {
+  app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+  });
+}
