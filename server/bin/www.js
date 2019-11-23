1424fef48cd0c98e155941138fde27ec78cacfb0
BuildTools <unconfigured@null.spigotmc.org>
2019-11-23 20:46:58 +0900

    added backup server to container, applied load balancing on nginx

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,12 +1,15 @@
 const cluster = require("cluster");
+const numContainers = 2;
 const numCPUs = require("os").cpus().length;
+const numWorkers = numCPUs / numContainers; // 코어수(4)를 컨테이너수(2)로 나누면 워커수(2)가 나옴
 
 const app = require("../index"); // 절대경로가 제대로 동작되지 않고 있음
 console.log(`SERVER PORT: ${process.env.NODE_PORT}`);
 
 if (cluster.isMaster) {
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork(); // 코어수만큼 워커 생성
+  console.log(`WORKER NUM: ${numWorkers}`);
+  for (let i = 0; i < numWorkers; i++) {
+    cluster.fork();
   }
   cluster.on("exit", (worker, code, signal) => {
     console.log(`Worker ${worker.process.pid} died.`);
